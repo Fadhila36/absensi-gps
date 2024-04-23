6a0ddc3d299ab31b44d8bbceb8ad50c1812c4606
fadhila36 <yuffa36@gmail.com>
2024-04-23 13:50:17 +0700

    add: Form Input dan Validasi Data Karyawan

--- resources/views/karyawan/index.blade.php
+++ resources/views/karyawan/index.blade.php
@@ -18,17 +18,49 @@
     <div class="div page-body">
         <div class="container-xl">
             <div class="row">
-                <div class="col-12">
+                <div class="col-12">Search & Pagination Data Karyawan
                     <div class="card">
                         <div class="card-body">
                             <div class="row">
+                                <div class="col-12">
+                                    @if (Session::has('success'))
+                                        <div class="alert alert-success">
+                                            {{ Session::get('success') }}
+                                        </div>
+                                    @endif
+                                    @if (Session::has('warning'))
+                                        <div class="alert alert-warning">
+                                            {{ Session::get('warning') }}
+                                        </div>
+                                    @endif
+                                </div>
+
+                            </div>
+                            <div class="row">
+                                <div class="col-12">
+                                    <a href="#" class="btn btn-primary" id="btnTambahKaryawan">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                                            viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
+                                            stroke-linecap="round" stroke-linejoin="round"
+                                            class="icon icon-tabler icons-tabler-outline icon-tabler-circle-plus">
+                                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                            <path d="M3 12a9 9 0 1 0 18 0a9 9 0 0 0 -18 0" />
+                                            <path d="M9 12h6" />
+                                            <path d="M12 9v6" />
+                                        </svg>
+                                        Tambah Data
+                                    </a>
+                                </div>
+                            </div>
+                            <div class="row mt-2">
                                 <div class="col-12">
                                     <form action="{{ url('/karyawan') }}" method="GET">
                                         <div class="row">
                                             <div class="col-6">
                                                 <div class="form-group">
                                                     <input type="text" class="form-control" id="nama_karyawan"
-                                                        name="nama_karyawan" placeholder="Cari Karyawan" value="{{ Request('nama_karyawan') }}">
+                                                        name="nama_karyawan" placeholder="Cari Karyawan"
+                                                        value="{{ Request('nama_karyawan') }}">
                                                 </div>
                                             </div>
                                             <div class="col-4">
@@ -36,7 +68,10 @@
                                                     <select name="kode_dept" id="kode_dept" class="form-select">
                                                         <option value="">Departemen</option>
                                                         @foreach ($departemen as $item)
-                                                            <option {{ Request('kode_dept') == $item->kode_dept ? 'selected' : '' }} value="{{ $item->kode_dept }}">{{ $item->nama_dept }}</option>
+                                                            <option
+                                                                {{ Request('kode_dept') == $item->kode_dept ? 'selected' : '' }}
+                                                                value="{{ $item->kode_dept }}">{{ $item->nama_dept }}
+                                                            </option>
                                                         @endforeach
                                                     </select>
                                                 </div>
@@ -108,4 +143,219 @@
             </div>
         </div>
     </div>
+
+    <div class="modal modal-blur fade" id="modal-addKaryawan" tabindex="-1" role="dialog" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">Tambah Data Karyawan</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <form action="{{ url('/karyawan/store') }}" method="POST" id="form-addKaryawan"
+                        enctype="multipart/form-data">
+                        @csrf
+                        <div class="row">
+                            <div class="col-12">
+                                <div class="input-icon mb-3">
+                                    <span class="input-icon-addon">
+                                        <!-- Download SVG icon from http://tabler-icons.io/i/user -->
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                                            viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
+                                            stroke-linecap="round" stroke-linejoin="round"
+                                            class="icon icon-tabler icons-tabler-outline icon-tabler-id-badge-2">
+                                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                            <path d="M7 12h3v4h-3z" />
+                                            <path
+                                                d="M10 6h-6a1 1 0 0 0 -1 1v12a1 1 0 0 0 1 1h16a1 1 0 0 0 1 -1v-12a1 1 0 0 0 -1 -1h-6" />
+                                            <path
+                                                d="M10 3m0 1a1 1 0 0 1 1 -1h2a1 1 0 0 1 1 1v3a1 1 0 0 1 -1 1h-2a1 1 0 0 1 -1 -1z" />
+                                            <path d="M14 16h2" />
+                                            <path d="M14 12h4" />
+                                        </svg> </span>
+                                    <input type="number" value="" id="nik" class="form-control"
+                                        name="nik"placeholder="Nik">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-12">
+                                <div class="input-icon mb-3">
+                                    <span class="input-icon-addon">
+                                        <!-- Download SVG icon from http://tabler-icons.io/i/user -->
+                                        <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24"
+                                            height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor"
+                                            fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                                            <path d="M8 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0"></path>
+                                            <path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2"></path>
+                                        </svg> </span>
+                                    <input type="text" value="" id="nama_lengkap" class="form-control"
+                                        name="nama_lengkap" placeholder="Nama Lengkap">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-12">
+                                <div class="input-icon mb-3">
+                                    <span class="input-icon-addon">
+                                        <!-- Download SVG icon from http://tabler-icons.io/i/user -->
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                                            viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
+                                            stroke-linecap="round" stroke-linejoin="round"
+                                            class="icon icon-tabler icons-tabler-outline icon-tabler-device-analytics">
+                                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                            <path
+                                                d="M3 4m0 1a1 1 0 0 1 1 -1h16a1 1 0 0 1 1 1v10a1 1 0 0 1 -1 1h-16a1 1 0 0 1 -1 -1z" />
+                                            <path d="M7 20l10 0" />
+                                            <path d="M9 16l0 4" />
+                                            <path d="M15 16l0 4" />
+                                            <path d="M8 12l3 -3l2 2l3 -3" />
+                                        </svg></span>
+                                    <input type="text" value="" id="jabatan" class="form-control"
+                                        name="jabatan" placeholder="Jabatan">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-12">
+                                <div class="input-icon mb-3">
+                                    <span class="input-icon-addon">
+                                        <!-- Download SVG icon from http://tabler-icons.io/i/user -->
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                                            viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
+                                            stroke-linecap="round" stroke-linejoin="round"
+                                            class="icon icon-tabler icons-tabler-outline icon-tabler-device-mobile">
+                                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                            <path
+                                                d="M6 5a2 2 0 0 1 2 -2h8a2 2 0 0 1 2 2v14a2 2 0 0 1 -2 2h-8a2 2 0 0 1 -2 -2v-14z" />
+                                            <path d="M11 4h2" />
+                                            <path d="M12 17v.01" />
+                                        </svg></span>
+                                    <input type="number" value="" id="no_hp" class="form-control"
+                                        name="no_hp" placeholder="No Handphone">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-12">
+                                <div class="form-label">Photo</div>
+                                <input type="file" name="foto" class="form-control">
+                            </div>
+                        </div>
+                        <div class="row mt-2">
+                            <div class="col-12">
+                                <select name="kode_dept" id="kode_dept" class="form-select">
+                                    <option value="">Departemen</option>
+                                    @foreach ($departemen as $item)
+                                        <option {{ Request('kode_dept') == $item->kode_dept ? 'selected' : '' }}
+                                            value="{{ $item->kode_dept }}">{{ $item->nama_dept }}
+                                        </option>
+                                    @endforeach
+                                </select>
+                            </div>
+                        </div>
+                        <div class="row mt-2">
+                            <div class="col-12">
+                                <div class="form-group">
+                                    <button class="btn btn-primary w-100">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                                            viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
+                                            stroke-linecap="round" stroke-linejoin="round"
+                                            class="icon icon-tabler icons-tabler-outline icon-tabler-send">
+                                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                            <path d="M10 14l11 -11" />
+                                            <path
+                                                d="M21 3l-6.5 18a.55 .55 0 0 1 -1 0l-3.5 -7l-7 -3.5a.55 .55 0 0 1 0 -1l18 -6.5" />
+                                        </svg>
+                                        Simpan
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
+
+@push('myscript')
+    <script>
+        $(function() {
+            $('#btnTambahKaryawan').on('click', function() {
+                $('#modal-addKaryawan').modal('show');
+            })
+
+            $('#form-addKaryawan').on('submit', function(e) {
+                let nik = $("#nik").val();
+                let nama_lengkap = $("#nama_lengkap").val();
+                let jabatan = $("#jabatan").val();
+                let no_hp = $("#no_hp").val();
+                let kode_dept = $("#form-addKaryawan").find("#kode_dept").val();
+                if (nik == "") {
+                    // alert('Nik harus diisi');
+                    Swal.fire({
+                        title: 'Warning',
+                        text: 'Nik harus diisi',
+                        icon: 'warning',
+                        confirmButtonText: 'Ok',
+                    }).then((result) => {
+                        if (result.isConfirmed) {
+                            $("#nik").focus();
+                        }
+                    });
+                    return false;
+                } else if (nama_lengkap == "") {
+                    Swal.fire({
+                        title: 'Warning',
+                        text: 'Nama harus diisi',
+                        icon: 'warning',
+                        confirmButtonText: 'Ok',
+                    }).then((result) => {
+                        if (result.isConfirmed) {
+                            $("#nama_lengkap").focus();
+                        }
+                    });
+                    return false;
+                } else if (jabatan == "") {
+                    Swal.fire({
+                        title: 'Warning',
+                        text: 'Jabatan harus diisi',
+                        icon: 'warning',
+                        confirmButtonText: 'Ok',
+                    }).then((result) => {
+                        if (result.isConfirmed) {
+                            $("#jabatan").focus();
+                        }
+                    });
+                    return false;
+                } else if (no_hp == "") {
+                    Swal.fire({
+                        title: 'Warning',
+                        text: 'Nomor HP harus diisi',
+                        icon: 'warning',
+                        confirmButtonText: 'Ok',
+                    }).then((result) => {
+                        if (result.isConfirmed) {
+                            $("#no_hp").focus();
+                        }
+                    });
+                    return false;
+                } else if (kode_dept == "") {
+                    Swal.fire({
+                        title: 'Warning',
+                        text: 'Kode departemen harus diisi',
+                        icon: 'warning',
+                        confirmButtonText: 'Ok',
+                    }).then((result) => {
+                        if (result.isConfirmed) {
+                            $("#kode_dept").focus();
+                        }
+                    });
+                    return false;
+                }
+            });
+        })
+    </script>
+@endpush
